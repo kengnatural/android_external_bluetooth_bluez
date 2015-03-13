a5b8af6fb734f681940459dc53f4af94d95e752c
András Oravecz <oraveczandrew@gmail.com>
2015-03-13 14:00:32 +0100

    secure fix

--- android/client/if-bt.c
+++ android/client/if-bt.c
@@ -241,14 +241,14 @@ static void pin_request_answer(char *reply)
 }
 
 static void pin_request_cb(bt_bdaddr_t *remote_bd_addr, bt_bdname_t *bd_name,
-								uint32_t cod)
+								uint32_t cod, uint8_t secure)
 {
 	/* Store for command completion */
 	bt_bdaddr_t2str(remote_bd_addr, last_remote_addr);
 	pin_request_addr = *remote_bd_addr;
 
-	haltest_info("%s: remote_bd_addr=%s bd_name=%s cod=%06x\n", __func__,
-					last_remote_addr, bd_name->name, cod);
+    haltest_info("%s: remote_bd_addr=%s bd_name=%s cod=%06x secure=%02x\n", __func__,
+                 last_remote_addr, bd_name->name, cod, secure);
 	terminal_prompt_for("Enter pin: ", pin_request_answer);
 }
 
